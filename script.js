1f19b619800e1bf3c44c7e42a0f51f00705d507f
Luke Allpress <lukeallpress@gmail.com>
2019-03-08 07:02:06 -0700

    2019 update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var baseField = document.querySelector("#base")
 var check = document.querySelector("input[name=choose]")
 var perf = document.querySelector("input[name=performance]")
 var edit1 = document.querySelector("#edit1")
-var edit2 = document.querySelector("#edit2")
+// var edit2 = document.querySelector("#edit2")
 var perfValue = 0
 var contract = 50091
 console.log(check)
@@ -28,7 +28,7 @@ baseField.addEventListener("keyup", function(event) {
 
 perf.addEventListener('change', e => {
   if (perf.checked) {
-    perfValue = 2750
+    perfValue = 4000
   } else {
     perfValue = 0
   }
@@ -42,23 +42,24 @@ edit1.addEventListener("keyup", function(event) {
   }
 });
 
-edit2.addEventListener("keyup", function(event) {
-  if (event.keyCode == 13) {
-    update()
-  }
-});
+// edit2.addEventListener("keyup", function(event) {
+//   if (event.keyCode == 13) {
+//     update()
+//   }
+// });
 
 function update() {
   base = parseFloat(baseField.value)
-  var per1 = (base + 1435) * 0.0375
-  var per2 = (base + 4120) * 0.0775
-  contract = base + 4120 + per2
-  var per301 = (contract + Number(edit2.value)) * 0.035
+  var per1 = (base + 800) * 0.035
+  // var per2 = (base + 4120) * 0.0775
+  contract = base + 800 + per1 + edit1.value
+  per301s = contract * 0.035
   document.querySelector("#per1").innerHTML = "$" + Math.round(per1)
-  document.querySelector("#base1").innerHTML = "$" + (base + 1435 + Math.round(per1) + Number(edit1.value))
-  document.querySelector("#per2").innerHTML = "$" + Math.round(per2)
-  document.querySelector("#base2").innerHTML = "$" + (Math.round(contract) + Number(edit2.value))
-  document.querySelector("#inc").innerHTML = Math.round((contract / base - 1) * 100) + "%"
+  document.querySelector("#base1").innerHTML = "$" + (base + 800 + Math.round(per1) + Number(edit1.value))
+  // document.querySelector("#per2").innerHTML = "$" + Math.round(per2)
+  // document.querySelector("#base2").innerHTML = "$" + (Math.round(contract) )
+  document.querySelector("#inc").innerHTML = Math.round((contract / base - 1) * 1000) / 10 + "%"
   document.querySelector("#per301").innerHTML = "$" + Math.round(per301)
-  document.querySelector("#contract301").innerHTML = "$" + Math.round(contract + 2600 + per301 + perfValue)
+  foo = Math.round(Number(contract) + 2600 + per301s + perfValue)
+  document.querySelector("#contract301").innerHTML = "$" + Math.round(Number(contract) + 2600 + per301s + perfValue)
 }
